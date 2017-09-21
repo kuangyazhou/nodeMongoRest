eb1bfd75af7f0309db17b4bdfcb7c7822bc90872
kuangyazhou <18707174713@139.com>
2017-09-21 14:47:53 +0800

    'index页面'

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/routes');
 
 routes(app);
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname + '/' })
+})
 
 // app.get('/', function(req, res) {
 //     res.send('fuck the king！！！');
 // })
 app.listen(port);
+app.use(function(req, res) {
+    res.status(404).send({ url: req.originalUrl + 'not found' });
+})
 console.log('todo list restful API server started on:' + port);
